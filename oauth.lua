76a026e7b3021cb86f3cf5844aeb6d3060eabb1a
Lerg <lvs.tourist@gmail.com>
2013-03-03 02:41:54 +0600

    Initial commit

---
+++ oauth.lua
@@ -0,0 +1,145 @@
+local _M = {}
+ 
+local crypto = require('crypto')
+local mime = require('mime')
+
+-- Replaces unsafe URL characters with %hh (two hex characters)
+local function encode_parameter(str)
+    return str:gsub('[^-%._~a-zA-Z0-9]',function(c)
+        return string.format('%%%02x',c:byte()):upper()
+    end)
+end
+
+local function sha1(str,key,binary)
+    binary = binary or false
+    return crypto.hmac(crypto.sha1,str,key,binary)
+end
+
+local function get_nonce()
+    return mime.b64(crypto.hmac(crypto.sha1,tostring(math.random()) .. 'random'
+        .. tostring(os.time()),'keyyyy'))
+end
+
+local function get_timestamp()
+    return tostring(os.time() + 1)
+end
+
+local function oAuthSign(url, method, args, consumer_secret)
+    local token_secret = args.oauth_token_secret or ''
+    args.oauth_token_secret = nil
+    local keys_and_values = {}
+
+    for key, val in pairs(args) do
+        table.insert(keys_and_values, {
+            key = encode_parameter(key),
+            val = encode_parameter(val) })
+    end
+ 
+    table.sort(keys_and_values, function(a,b)
+        if a.key < b.key then
+            return true
+        elseif a.key > b.key then
+            return false
+        else
+            return a.val < b.val
+        end
+    end)
+    
+    local key_value_pairs = {}
+ 
+    for _, rec in pairs(keys_and_values) do
+        table.insert(key_value_pairs, rec.key .. '=' .. rec.val)
+    end
+    
+    local query_string_except_signature = table.concat(key_value_pairs, '&')
+
+    local sign_base_string = method .. '&' .. encode_parameter(url) .. '&'
+        .. encode_parameter(query_string_except_signature)
+
+    local key = encode_parameter(consumer_secret) .. '&' .. encode_parameter(token_secret)
+    local hmac_binary = sha1(sign_base_string, key, true)
+
+    local hmac_b64 = mime.b64(hmac_binary)
+    local query_string = query_string_except_signature .. '&oauth_signature=' .. encode_parameter(hmac_b64)
+ 
+    if method == 'GET' then
+       return url .. '?' .. query_string
+    else
+        return query_string
+    end
+end
+
+function _M.getRequestToken(consumer_key, token_ready_url, request_token_url, consumer_secret, callback)
+    local post_data = {
+        oauth_consumer_key     = consumer_key,
+        oauth_timestamp        = get_timestamp(),
+        oauth_version          = '1.0',
+        oauth_nonce            = get_nonce(),
+        oauth_callback         = token_ready_url,
+        oauth_signature_method = 'HMAC-SHA1'}
+    
+    local post_data = oAuthSign(request_token_url, 'POST', post_data, consumer_secret)
+    _M.rawPostRequest(request_token_url, post_data, function (event)
+            if not event.isError then
+                local response = event.response
+                callback{isError = false, token = response:match('oauth_token=([^&]+)'), token_secret = response:match('oauth_token_secret=([^&]+)')}
+            else
+                callback{isError = true}
+            end
+        end)
+end
+
+function _M.getAccessToken(token, verifier, token_secret, consumer_key, consumer_secret, access_token_url, callback)
+    local post_data = {
+        oauth_consumer_key = consumer_key,
+        oauth_timestamp    = get_timestamp(),
+        oauth_version      = '1.0',
+        oauth_nonce        = get_nonce(),
+        oauth_token        = token,
+        oauth_token_secret = token_secret,
+        oauth_verifier     = verifier,
+        oauth_signature_method = 'HMAC-SHA1'}
+    local post_data = oAuthSign(access_token_url, 'POST', post_data, consumer_secret)
+    _M.rawPostRequest(access_token_url, post_data, function (event)
+            if not event.isError then
+                callback{isError = false, response = event.response}
+            else
+                callback{isError = true}
+            end
+        end)
+end
+
+function _M.makeRequest(url, body, consumer_key, token, consumer_secret, token_secret, method, callback)
+    local post_data = {
+        oauth_consumer_key = consumer_key,
+        oauth_nonce        = get_nonce(),
+        oauth_signature_method = 'HMAC-SHA1',
+        oauth_token        = token,
+        oauth_timestamp    = get_timestamp(),
+        oauth_version      = '1.0',
+        oauth_token_secret = token_secret}
+    for i=1, #body do
+        post_data[body[i].key] = body[i].value
+    end
+    local post_data = oAuthSign(url, method, post_data, consumer_secret)
+    local result
+    if method == 'POST' then
+        _M.rawPostRequest(url, post_data, callback)
+    else
+        _M.rawGetRequest(post_data, callback)
+    end
+end
+
+function _M.rawGetRequest(url, callback)
+    network.request(url, 'GET', callback)
+end
+
+function _M.rawPostRequest(url, rawdata, callback)
+    network.request(url, 'POST', callback, {
+        headers = {
+            ['Content-Type'] = 'application/x-www-form-urlencoded', 
+            ['Content-Length'] = string.len(rawdata)},
+        body = rawdata})
+end
+
+return _M
---
+++ twitter.lua
@@ -0,0 +1,205 @@
+local _M = {}
+
+local oAuth = require('oauth')
+
+_M.consumer_key = 'your key here'
+_M.consumer_secret = 'your secret here'
+_M.webURL = 'your web site callback url here'
+
+_M.twitter_request_token_secret = nil
+
+_M.access_token = nil
+_M.access_token_secret = nil
+_M.user_id = 0
+_M.screen_name = ''
+
+_M.action = ''
+
+_M.postMessage = ''
+_M.delegate = ''
+
+local function responseToTable(str, delimeters)
+	local obj = {}
+
+	while str:find(delimeters[1]) ~= nil do
+		if #delimeters > 1 then
+			local key_index = 1
+			local val_index = str:find(delimeters[1])
+			local key = str:sub(key_index, val_index - 1)
+	
+			str = str:sub((val_index + delimeters[1]:len()))
+	
+			local end_index
+			local value
+	
+			if str:find(delimeters[2]) == nil then
+				end_index = str:len()
+				value = str
+			else
+				end_index = str:find(delimeters[2])
+				value = str:sub(1, (end_index - 1))
+				str = str:sub((end_index + delimeters[2]:len()), str:len())
+			end
+			obj[key] = value
+		else
+	
+			local val_index = str:find(delimeters[1])
+			str = str:sub((val_index + delimeters[1]:len()))
+	
+			local end_index
+			local value
+	
+			if str:find(delimeters[1]) == nil then
+				end_index = str:len()
+				value = str
+			else
+				end_index = str:find(delimeters[1])
+				value = str:sub(1, (end_index - 1))
+				str = str:sub(end_index, str:len())
+			end
+			
+			obj[#obj + 1] = value
+		end
+	end
+	
+	return obj
+end
+
+function _M.listener(event)
+	local url = event.url
+    local self = _M 
+	if url:find(self.webURL) then
+        if url:find('oauth_token') then
+            url = url:sub(url:find('?') + 1, url:len())
+            local authorize_response = responseToTable(url, {'=', '&'})
+
+            oAuth.getAccessToken(authorize_response.oauth_token,
+                authorize_response.oauth_verifier, self.twitter_request_token_secret,
+                self.consumer_key, self.consumer_secret, 'https://api.twitter.com/oauth/access_token', function (event)
+                        if not event.isError then
+                            local access_response = responseToTable(event.response, {'=', '&'})
+                            self.access_token = access_response.oauth_token
+                            self.access_token_secret = access_response.oauth_token_secret
+                            self.user_id = access_response.user_id
+                            self.screen_name = access_response.screen_name
+                            
+                            -- New api calls must be out in here as well
+                            if self.action == 'tweet' then
+                                self:apiTweet()
+                            elseif self.action == 'followers' then
+                                self:apiGetFollowers()
+                            end
+                            self.action = ''
+                         else
+                            print('Twitter getAccessToken Error')
+                         end
+                    end)
+        else
+            if type(self.delegate.twitterCancel) == 'function' then
+                self.delegate.twitterCancel()
+            end
+        end
+        native.cancelWebPopup()
+        return false
+	end
+    return true
+end
+
+function _M:apiLogin()
+    if not self.consumer_key or not self.consumer_secret then
+        if type(self.delegate.twitterFailed) == 'function' then
+            self.delegate.twitterFailed()
+        end
+        return
+    end
+
+    oAuth.getRequestToken(self.consumer_key, self.webURL, 'https://twitter.com/oauth/request_token', self.consumer_secret, function (event)
+            if not event.isError then
+                local twitter_request_token = event.token
+                self.twitter_request_token_secret = event.token_secret
+                if not twitter_request_token then
+                    if type(self.delegate.twitterFailed) == 'function' then
+                        self.delegate.twitterFailed()
+                    end
+                    return
+                end
+                -- Full screen WebView
+                native.showWebPopup(_L, _T, _R - _L, _B - _T, 'https://api.twitter.com/oauth/authorize?oauth_token=' .. twitter_request_token, {urlRequest = _M.listener})
+             else
+                 print('Twitter getRequestToken Error')
+                 if type(self.delegate.twitterFailed) == 'function' then
+                    self.delegate.twitterFailed()
+                 end
+             end
+        end)
+end
+
+function _M:apiTweet()
+    local params = {
+        key = 'status',
+        value = self.postMessage}
+    
+    oAuth.makeRequest('https://api.twitter.com/1/statuses/update.json',
+        params, self.consumer_key, self.access_token, self.consumer_secret, self.access_token_secret, 'POST', function (event)
+            if not event.isError then
+                if type(self.delegate.twitterSuccess) == 'function' then
+                    self.delegate.twitterSuccess()
+                end
+             else
+                 print('Twitter Tweet Error')
+                 if type(self.delegate.twitterFailed) == 'function' then
+                    self.delegate.twitterFailed()
+                 end
+             end
+        end)
+end
+
+function _M:tweet(del, msg)
+    self.action = 'tweet'
+	self.postMessage = msg
+	self.delegate = del
+	
+	if not self.access_token then
+        self:apiLogin()
+	else
+		self:apiTweet()
+	end
+end
+
+function _M:apiGetFollowers()
+    local params = {
+        user_id = self.user_id,
+        skip_status = true,
+        include_user_entities = false}
+    
+    oAuth.makeRequest('https://api.twitter.com/1.1/followers/list.json',
+        params, self.consumer_key, self.access_token, self.consumer_secret, self.access_token_secret, 'GET', function (event)
+            if not event.isError then
+                if type(self.delegate.twitterSuccess) == 'function' then
+                    self.delegate.twitterSuccess(event.response)
+                end
+             else
+                 print('Twitter Tweet Error')
+                 if type(self.delegate.twitterFailed) == 'function' then
+                    self.delegate.twitterFailed()
+                 end
+             end
+        end)
+end
+
+function _M:getFollowers(del)
+    self.action = 'followers'
+    self.delegate = del
+    if not self.access_token then
+        self:apiLogin()
+	else
+		self:apiGetFollowers()
+	end
+end
+
+function _M.getPhotoUrl(id)
+    return 'https://api.twitter.com/1/users/profile_image?id=' .. id .. '&size=bigger'
+end
+
+
+return _M
